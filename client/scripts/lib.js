2e88bd75959901100b79149a7b105e950dcb2841
Soulike <316413310@qq.com>
2017-08-18 13:04:02 +0800

    change domain

--- client/scripts/lib.js
+++ client/scripts/lib.js
@@ -8,7 +8,8 @@ const STATUS_ICONS = {
 	WATCHING: 'glyphicon-eye-open',
 	LEAVE: 'glyphicon-glass',
 };
-const [DOMAIN, PORT] = ['127.0.0.1', 80];
+//const [DOMAIN, PORT] = ['127.0.0.1', 80];
+const [DOMAIN, PORT] = ['47.94.152.219', 80];
 
 const MESSAGE_MAX_LENGTH = 50;
 
